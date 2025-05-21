0c85fa7f6506fdd289205d48e793b8783bcbf606
Andrew Craigie <andrew@acraigie.com>
2025-05-21 11:25:42 +0100

    Uncommented Paint_NewImage

--- esphome/components/gc9a01acustom/gc9a01acustom_display.cpp
+++ esphome/components/gc9a01acustom/gc9a01acustom_display.cpp
@@ -30,7 +30,7 @@ void GC9A01ACUSTOMDisplay::setup() {
   ESP_LOGD(TAG, "DEV_Module_Init succeeded");
 
   LCD_1IN28_Init(HORIZONTAL);
-  // Paint_NewImage(reinterpret_cast<UBYTE *>(this->framebuffer_), LCD_1IN28.WIDTH, LCD_1IN28.HEIGHT, 0, WHITE);
+  Paint_NewImage(reinterpret_cast<UBYTE *>(this->framebuffer_), LCD_1IN28.WIDTH, LCD_1IN28.HEIGHT, 0, WHITE);
   // Paint_SetScale(65);
   // Paint_SetRotate(ROTATE_0);
   // Paint_Clear(WHITE);
