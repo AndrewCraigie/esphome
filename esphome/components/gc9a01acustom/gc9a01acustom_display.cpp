e04bcec03c9affcb2670a6e59a1192651d99ae78
Andrew Craigie <andrew@acraigie.com>
2025-05-21 12:00:28 +0100

    Added a call to LCD_1in28_Clear in setup

--- esphome/components/gc9a01acustom/gc9a01acustom_display.cpp
+++ esphome/components/gc9a01acustom/gc9a01acustom_display.cpp
@@ -30,10 +30,12 @@ void GC9A01ACUSTOMDisplay::setup() {
   ESP_LOGD(TAG, "DEV_Module_Init succeeded");
 
   LCD_1IN28_Init(HORIZONTAL);
-  Paint_NewImage(reinterpret_cast<UBYTE *>(this->framebuffer_), LCD_1IN28.WIDTH, LCD_1IN28.HEIGHT, 0, WHITE);
-  Paint_SetScale(65);
-  Paint_SetRotate(ROTATE_0);
-  Paint_Clear(WHITE);
+  LCD_1IN28_Clear(this->framebuffer_, WHITE);
+
+  // Paint_NewImage(reinterpret_cast<UBYTE *>(this->framebuffer_), LCD_1IN28.WIDTH, LCD_1IN28.HEIGHT, 0, WHITE);
+  // Paint_SetScale(65);
+  // Paint_SetRotate(ROTATE_0);
+  // Paint_Clear(WHITE);
 
   // LCD_1IN28_Clear(this->framebuffer_, 0x07E0);  // GREEN
 }
