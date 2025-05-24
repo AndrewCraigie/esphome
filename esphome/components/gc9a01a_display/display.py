523a2d7ac75614f0273cdf77154ef23032c81ef3
Andrew Craigie <andrew@acraigie.com>
2025-05-24 21:09:06 +0100

    Commented register_display

--- esphome/components/gc9a01a_display/display.py
+++ esphome/components/gc9a01a_display/display.py
@@ -43,7 +43,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
-    await display.register_display(var, config)
+    # await display.register_display(var, config)
 
     dc = await cg.gpio_pin_expression(config[CONF_DC_PIN])
     cg.add(var.set_dc_pin(dc))
